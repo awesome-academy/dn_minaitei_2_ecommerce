ff1de0ff12c64b824a8e0627b1506299623a3086
Daiki <hothanhhung131@gmail.com>
2024-03-25 11:25:03 +0700

    add toast for flash message

--- app/javascript/custom/order.js
+++ app/javascript/custom/order.js
@@ -1,26 +1,31 @@
-document.addEventListener("turbo:load", function() {
-  var orderButton = document.getElementById("order_button");
-  var orderPopup = document.getElementById("order_popup");
+document.addEventListener('turbo:load', function () {
+  var orderButton = document.getElementById('order_button')
+  var orderPopup = document.getElementById('order_popup')
 
-  orderButton.addEventListener("click", function() {
-    if (orderPopup.style.display === "none" || orderPopup.style.display === "") {
-      orderPopup.style.display = "block";
-      localStorage.setItem('orderPopupVisible', 'true');
-    } else {
-      orderPopup.style.display = "none";
-      localStorage.setItem('orderPopupVisible', 'false');
-    }
-  });
+  if (orderButton || orderPopup) {
+    orderButton.addEventListener('click', function () {
+      if (
+        orderPopup.style.display === 'none' ||
+        orderPopup.style.display === ''
+      ) {
+        orderPopup.style.display = 'block'
+        localStorage.setItem('orderPopupVisible', 'true')
+      } else {
+        orderPopup.style.display = 'none'
+        localStorage.setItem('orderPopupVisible', 'false')
+      }
+    })
 
-  window.addEventListener("click", function(event) {
-    if (event.target == orderPopup) {
-      orderPopup.style.display = "none";
-    }
-  });
+    window.addEventListener('click', function (event) {
+      if (event.target == orderPopup) {
+        orderPopup.style.display = 'none'
+      }
+    })
 
-  window.addEventListener("keydown", function(event) {
-    if (event.key === "Escape") {
-      orderPopup.style.display = "none";
-    }
-  });
-});
+    window.addEventListener('keydown', function (event) {
+      if (event.key === 'Escape') {
+        orderPopup.style.display = 'none'
+      }
+    })
+  }
+})
